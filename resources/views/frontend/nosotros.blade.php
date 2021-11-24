d34b8289029fa468add179d960f9e43605f2372a
Carlos <hi@carlosmacha.com>
2021-11-24 12:40:09 -0500

    Cambios de Prueba

--- resources/views/frontend/nosotros.blade.php
+++ resources/views/frontend/nosotros.blade.php
@@ -5,7 +5,7 @@
 <div id="breadcrumb">
 			<div class="container">
 				<ul>
-					<li><a href="#">Home</a></li>
+					<li><a href="#">Inicio</a></li>
 					<li><a href="#">Category</a></li>
 					<li>Page active</li>
 				</ul>
